bf0579f10d3c7973b05c39a320ec4dc61bb8ca13
unknown <David@.tsp2internal.local>
2011-07-11 17:31:56 -0700

    Ticket #15 #16 Moved login session writing code from app_controller to user_controller.  Created loginredirect controller action in users_controller.  Added logic to log users in after they registered and direct them to a different page on this first login.

--- app/app_controller.php
+++ app/app_controller.php
@@ -2,7 +2,7 @@
 class AppController extends Controller {
     var $components = array('Acl', 'Auth', 'Session', 'Cookie');
     var $helpers = array('Html', 'Form', 'Session', 'AssetCompress.AssetCompress', 'Javascript');
-    
+ 
     var $view = 'Theme';
     //var $theme = 'cake_default';
 	  //Define all global variables here
@@ -17,31 +17,9 @@ class AppController extends Controller {
 			);
         $this->Auth->loginAction = array('controller' => 'users', 'action' => 'login');
         $this->Auth->logoutRedirect = array('controller' => 'users', 'action' => 'logout');
-        $this->Auth->loginRedirect = array('controller' => 'users', 'action' => 'index');
-		//$this->Auth->loginRedirect = '/';
-		//$this->Auth->autoRedirect = false;
+        $this->Auth->loginRedirect = array('controller' => 'users', 'action' => 'loginredirect');
 		$this->Cookie->name = 'TravelTipping';
-		
-		//Set Session variables.  I'm not sure if this is the right place to do this?
-		//I'm hoping that the !User.role_id will prevent it from running this code every pageload.
-		if ($this->Session->read('Auth.User') && !$this->Session->read('User.role_id')) {
-		$user = $this->Session->read('Auth.User');
-		if($user['role_id'] == Configure::Read('Role.Merchant_ID'))
-		{
-			$merchant = $this->Merchant->find('first',
-				array('conditions' => array('Merchant.user_id' => $user['id'])));
-			$this->Session->write('Merchant.id', $merchant['Merchant']['id']);
-		} 
-		
-		if($user['role_id'] == Configure::Read('Role.Traveler_ID'))
-		{
-			$traveler = $this->Traveler->find('first',
-				array('conditions' => array('Traveler.user_id' => $user['id'])));
-			$this->Session->write('Traveler.id', $traveler['Traveler']['id']);
-		}
-		$this->Session->write('User.role_id', $user['role_id']);
-		$this->Session->write('User.id', $user['id']);
-	}
+
     }
 }
 ?>
