72ddb677481adbce06c22376cb9b5dcb557fb510
David <dave@traveltipping.com>
2011-09-17 16:18:38 -0700

    Testing how committing and pulling affects branch

--- app/controllers/deals_controller.php
+++ app/controllers/deals_controller.php
@@ -14,7 +14,7 @@ class DealsController extends AppController {
 
 	//var $helpers = array('Text','Js', 'Html', 'Ajax', 'Javascript', 'Form');
 	 //var $helpers = array('Html','Ajax','Javascript');
-	
+	//test
 	var $paginate = array('Deal'=>array('group'=>'Deal.id'));
 /*
  * Index
